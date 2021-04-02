e8740a6d2066f75fbaaf580b2f0b298e7d025692
Si Taggart <me@simontaggart.com>
2021-04-02 13:22:43 -0700

    fix(theme): set a background color for the base element
    Set the root theme provider to have a background color, so that background body color is applied when theming.

--- packages/paste-theme/src/styles/base.ts
+++ packages/paste-theme/src/styles/base.ts
@@ -1,6 +1,7 @@
 import {css} from '@twilio-paste/styling-library';
 
 export const pasteBaseStyles = css({
+  backgroundColor: 'colorBackgroundBody',
   color: 'colorText',
   fontSize: 'fontSize30',
   fontFamily: 'fontFamilyText',
